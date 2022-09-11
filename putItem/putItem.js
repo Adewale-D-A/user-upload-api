99a7eb2c05d5c8c91935b5a0332ee85c12dd4fb1
adewale-d-a <adewale-d-a.com>
2022-09-11 22:06:56 +0100

    putItem into s3 and dynamodb

--- putItem/putItem.js
+++ putItem/putItem.js
@@ -3,57 +3,103 @@ const express = require("express");
 const router = express.Router();
 const AWS = require("aws-sdk");
 const jwt = require("jsonwebtoken");
+const multer = require("multer");
+const path = require("path");
+const axios = require("axios");
+const fs = require("fs");
+
+//set storage folder and upload name
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./imageUpload");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage: storage });
+
+//import s3putObject Function
+const { presignedPUTurl } = require("../BucketFunctions/S3BucketMethods");
 
 const { credentials, jwtSecretKey } = require("../config");
 const doClient = new AWS.DynamoDB.DocumentClient(credentials);
 
 const uniqueId = uuidv4();
 
-router.put("/putItem", (req, res) => {
-  const { itemDescription, imageName, requestToken } = req.body;
-
-  // const requestToken = req.cookies.token;
+router.put("/putItem", upload.single("imageUpload"), (req, res) => {
+  const { itemDescription } = req.body;
+  const requestToken = req.cookies.token;
   const verify = jwt.verify(requestToken, jwtSecretKey);
   const tableNameSpace = verify.dynamoDBuserTable;
   const dateTime = new Date().toISOString();
 
-  const payload = {
-    id: uniqueId,
-    description: itemDescription,
-    image: imageName,
-    dateCreated: dateTime,
-    dateUpdated: dateTime,
-  };
+  if (itemDescription && req.file.filename) {
+    const payload = {
+      id: uniqueId,
+      description: itemDescription,
+      image: req.file.filename,
+      dateCreated: dateTime,
+      dateUpdated: dateTime,
+    };
 
-  if (itemDescription && imageName) {
-    doClient.put(
-      {
-        TableName: tableNameSpace,
-        Item: payload,
-      },
-      (err, result) => {
-        if (err) {
-          res.status(400).send({
-            success: false,
-            message: "could not input new record into DB",
-          });
-        }
-        if (result) {
-          res.status(200).send({
-            success: true,
-            message: "item succesfully inputed into DynamoDB",
-            uniqueId,
-          });
-        }
-      }
+    const presignedUrl = presignedPUTurl(
+      "node-server-bucket",
+      req.file.filename,
+      1000 * 60
     );
+
+    fs.readFile(`./imageUpload/${req.file.filename}`, (err, result) => {
+      if (err) {
+        res.status(400).send({
+          success: false,
+          message: "image could not be read",
+        });
+      }
+      if (result) {
+        axios
+          .put(presignedUrl, result)
+          .then((response) => {
+            doClient.put(
+              {
+                TableName: tableNameSpace,
+                Item: payload,
+              },
+              (err, result) => {
+                if (err) {
+                  res.status(400).send({
+                    success: false,
+                    message: "could not input new record into DB",
+                  });
+                }
+                if (result) {
+                  res.status(200).send({
+                    success: true,
+                    message: "record upload into dynamoDB is successfully ",
+                    data: {
+                      imagePath: req.file.filename,
+                    },
+                  });
+                }
+              }
+            );
+          })
+          .catch((error) => {
+            res.status(400).send({
+              success: false,
+              message: "image could not be uploaded so s3",
+            });
+          });
+      }
+    });
   } else {
     res.status(400).send({
       success: false,
-      message: "invalid request body",
+      message: "invalid request body, please upload through formData",
       data: {
-        itemDescription: "sample description",
-        imageName: "image to upload",
+        itemDescription: "post description is required",
+        imageUpload: "'imageUpload:' --> for the image name is required",
       },
     });
   }
--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require("express");
+const app = express();
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const multer = require("multer");
-const path = require("path");
-const axios = require("axios");
-const fs = require("fs");
 
-const create = require("./createTable/createTable");
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: ["*", "http://localhost:3000"],
+    credentials: true,
+  })
+);
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 const delTable = require("./deleteTable/deleteTable");
 const delItem = require("./deleteItem/deleteItem");
 const getAll = require("./getAllItems/getAllItems");
@@ -14,34 +20,6 @@ const get = require("./getItem/getItem");
 const put = require("./putItem/putItem");
 const query = require("./queryTable/queryTable");
 const update = require("./updateItem/updateItem");
-
-const app = express();
-
-//import s3putObject Function
-const { presignedPUTurl } = require("./BucketFunctions/S3BucketMethods");
-
-//set storage folder and upload name
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "./imageUpload");
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
-  },
-});
-
-const upload = multer({ storage: storage });
-
-app.use(
-  cors({
-    origin: ["*", "http://localhost:3000"],
-    credentials: true,
-  })
-);
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(cookieParser());
 
 //delete table endpoint
 app.use("/deleteTable", delTable);
@@ -58,47 +36,8 @@ app.use("/query", query);
 //update item endpoint
 app.use("/update", update);
 
-app.post("/", upload.single("avatar"), (req, res) => {
-  // const token = req.cookies.token;
-  // console.log(token);
-  // console.log(req.file); //is the `avatar` file
-
-  console.log(req.body);
-
-  const presignedUrl = presignedPUTurl(
-    "node-server-bucket",
-    req.file.filename,
-    1000 * 60
-  );
-
-  fs.readFile(`./imageUpload/${req.file.filename}`, (err, result) => {
-    if (err) {
-      res.status(400).send({
-        success: false,
-        message: "image could not be read",
-      });
-    }
-    if (result) {
-      axios
-        .put(presignedUrl, result)
-        .then((response) => {
-          console.log(response);
-          res.status(200).send({
-            success: true,
-            message: "image uploaded successfuly",
-            data: {
-              imagePath: req.file.filename,
-            },
-          });
-        })
-        .catch((error) => {
-          res.status(400).send({
-            success: false,
-            message: "image could not be uploaded",
-          });
-        });
-    }
-  });
+app.post("/", (req, res) => {
+  res.status(200).send("Welcome to user upload landing");
 });
 
 app.listen(4000, () => {
